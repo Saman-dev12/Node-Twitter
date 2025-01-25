b8c4a56fd865a7a4afb548d9395386d0be517113
samanpreets84 <132365157+Saman-dev12@users.noreply.github.com>
2025-01-25 21:17:02 +0530

    Lets deploy

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const { TwitterApi } = require("twitter-api-v2");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const twitterClient = new TwitterApi({
   appKey: process.env.TWITTER_API_KEY,
@@ -9,35 +8,25 @@ const twitterClient = new TwitterApi({
   accessSecret: process.env.ACCOUNT_TOKEN_SECRET,
 });
 
-async function generateTweetUsingAI() {
-  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-  const prompt = `I want to post a humorous tweet on my account. The tweet should be tech-related, vary in length, and be both humorous and informative at times. People should like it. Return only the tweet. Do not return anything else. Don't add hashtags. let the jokes for the genz type people mostly related to Full stack development,Web3 and AI and never ever give the same or even similar tweet`;
-
-  const result = await model.generateContent(prompt, {
-    maxTokens: 100,
-    temperature: 0.95,
-    topP: 0.95,
-    topK: 40,
-  });
-  console.log(result.response.text());
-  return result.response.text();
-}
-
-async function generateTweet() {
+async function getTweetFromCfBackend() {
   try {
-    const res = await generateTweetUsingAI();
-    return res;
+    const response = await fetch("https://tweet.samanpreets84.workers.dev", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+    const data = await response.json();
+    console.log(data.tweet);
+    return data.tweet;
   } catch (error) {
-    console.error("Error generating tweet:", error);
-    return null;
+    console.error("Error fetching tweet:", error);
   }
 }
 
 async function postTweet() {
   try {
-    const tweetContent = await generateTweet();
+    const tweetContent = await getTweetFromCfBackend();
     if (tweetContent) {
       await twitterClient.v2.tweet(tweetContent);
       console.log("Tweet posted:", tweetContent);
